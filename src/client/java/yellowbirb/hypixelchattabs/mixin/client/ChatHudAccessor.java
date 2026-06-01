7a9a5ed6ed43dd7bd361fc1c80625605b586dc7a
Pinkcommando <pinkcommando@gmail.com>
2026-06-01 18:49:22 -0400

    bump version, minor changes

---
+++ src/client/java/yellowbirb/hypixelchattabs/mixin/client/ChatHudAccessor.java
@@ -0,0 +1,11 @@
+package yellowbirb.hypixelchattabs.mixin.client;
+
+import net.minecraft.client.gui.hud.ChatHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(ChatHud.class)
+public interface ChatHudAccessor {
+	@Invoker("getHeight")
+	int invokeGetHeight();
+}
--- src/client/java/yellowbirb/hypixelchattabs/mixin/client/ChatScreenMixin.java
+++ src/client/java/yellowbirb/hypixelchattabs/mixin/client/ChatScreenMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.input.KeyInput;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -29,27 +30,45 @@ public abstract class ChatScreenMixin extends Screen {
     private void onInit(CallbackInfo ci) {
         MinecraftClient client = MinecraftClient.getInstance();
         ChatHud hud = client.inGameHud.getChatHud();
+        int x = 5;
         for (Tab chatTab : Tab.values()) {
-            String message = "X";
-            switch (chatTab) {
-                case ALL -> message = "A";
-                case PARTY -> message = "P";
-                case GUILD -> message = "G";
-                case PRIVATE -> message = "PM";
-                case COOP -> message = "CC";
-            }
-            ButtonWidget tabButton = ButtonWidget.builder(Text.literal(message), (btn) -> {
+            String label = switch (chatTab) {
+                case ALL -> "All";
+                case PARTY -> "Party";
+                case GUILD -> "Guild";
+                case PRIVATE -> "DM";
+                case COOP -> "CO";
+            };
+            int btnWidth = client.textRenderer.getWidth(label) + 10;
+            ButtonWidget tabButton = ButtonWidget.builder(Text.literal(label), (btn) -> {
                 HypixelChatTabsClient.tab = chatTab;
                 hud.reset();
                 client.send(() -> setFocused(chatField));
-            }).dimensions(5 + chatTab.ordinal() * 22, this.height - hud.getHeight() - 40 - 20 - 5, 20, 20).build();
+            }).dimensions(x, this.height - ((ChatHudAccessor) hud).invokeGetHeight() - 40 - 20 - 5, btnWidth, 20).build();
+            x += btnWidth + 2;
 
             addDrawableChild(tabButton);
         }
     }
 
+    @Inject(at = @At("HEAD"), method = "sendMessage", cancellable = true)
+    private void onSendMessage(String message, boolean addToHistory, CallbackInfo ci) {
+        if (message.startsWith("/")) return;
+        String prefix = switch (HypixelChatTabsClient.tab) {
+            case ALL -> null;
+            case PARTY -> "/pc ";
+            case GUILD -> "/gc ";
+            case PRIVATE -> null;
+            case COOP -> "/cc ";
+        };
+        if (prefix != null) {
+            ci.cancel();
+            ((ChatScreen) (Object) this).sendMessage(prefix + message, addToHistory);
+        }
+    }
+
     @Inject(at = @At("HEAD"), method = "keyPressed")
-    private void onKeyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
+    private void onKeyPressed(KeyInput keyInput, CallbackInfoReturnable<Boolean> cir) {
         setFocused(chatField);
     }
 }
